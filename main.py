ce22caae4ddf255a8579353f26fcf64236812aaa
chenyy230 <8665254@qq.com>
2023-05-01 19:29:04 +0800

    update3

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # 按 Shift+F10 执行或将其替换为您的代码。
 # 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
 
-
+print('three commit')
 def print_hi(name):
     # 在下面的代码行中使用断点来调试脚本。
     print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
@@ -17,3 +17,5 @@ if __name__ == '__main__':
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
 print('say hello')
+
+

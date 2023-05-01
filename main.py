5d0b1aade958aff20ea5315607a3f7e62cd98e29
chenyy230 <8665254@qq.com>
2023-05-01 20:12:08 +0800

    update7

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def print_hi(name):
     # 在下面的代码行中使用断点来调试脚本。
     print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
 
-
+print('seven commit')
 print('one commit')
 
 # 按间距中的绿色按钮以运行脚本。
@@ -26,3 +26,8 @@ print('four commit')
 
 
 print('five commit')
+
+
+
+
+
